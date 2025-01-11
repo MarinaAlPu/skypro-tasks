97c60760ba7559eb4f0bdac04586bd59dcd2497e
Владислав Чечулин <v.chechulin@vk.team>
2025-01-11 15:38:45 +0300

    init api func

---
+++ modules/api.js
@@ -0,0 +1,39 @@
+const host = 'https://wedev-api.sky.pro/api/v2/todos'
+
+const token = 'Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k'
+
+export function getTodos() {
+    return fetch(host, {
+        method: 'GET',
+        headers: {
+            Authorization: token,
+        },
+    }).then((response) => {
+        return response.json()
+    })
+}
+
+export function deleteTodo({ id }) {
+    return fetch(`${host}/${id}`, {
+        method: 'DELETE',
+        headers: {
+            Authorization: token,
+        },
+    }).then((response) => {
+        return response.json()
+    })
+}
+
+export function postTodo({ text }) {
+    return fetch(host, {
+        method: 'POST',
+        headers: {
+            Authorization: token,
+        },
+        body: JSON.stringify({
+            text,
+        }),
+    }).then((response) => {
+        return response.json()
+    })
+}
