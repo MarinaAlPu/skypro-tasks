8cc548c2710955308d1b31312c36ca0b5a0c45dd
MarinaAlPu <marina.pudovkina13@gmail.com>
2025-08-16 22:31:00 +0500

    Add get, post, delete task

--- index.js
+++ index.js
@@ -1,26 +1,52 @@
 import { renderTasks } from './modules/renderTasks.js'
-import { tasks } from './modules/tasks.js'
+import { updateTasks } from './modules/tasks.js'
 
-renderTasks()
+fetch("https://wedev-api.sky.pro/api/todos", {
+    method: 'GET',
+})
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        // console.log(data);
+        updateTasks(data.todos)
+        renderTasks() // вызываем рендер после того,как получили данные от сервера о существующих задачах
+    });
+
 
 const button = document.getElementById('add')
 const input = document.getElementById('field')
 
-button.addEventListener('click', () => {
-    input.classList.remove('error')
+button.addEventListener("click", () => {
+    input.classList.remove("error");
 
-    if (input.value === '') {
-        input.classList.add('error')
-        return
+    if (input.value === "") {
+        input.classList.add("error");
+        return;
     }
+
+    // const newTask = { text: formateInput(input.value) };
+    // console.log("Новая задача: ", newTask);
 
     const newTask = {
         text: input.value.replaceAll('<', '&lt;').replaceAll('>', '&gt;'),
     }
 
-    tasks.push(newTask)
+    // tasks.push(newTask);
 
-    input.value = ''
+    // создание новой задачи через апи
+    fetch("https://wedev-api.sky.pro/api/todos", {
+        method: 'POST',
+        body: JSON.stringify(newTask),
+    })
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data.todos)
+            updateTasks(data.todos)
+            renderTasks()
+        })
 
-    renderTasks()
-})
+    input.value = "";
+});
--- modules/tasks.js
+++ modules/tasks.js
@@ -1,11 +1,10 @@
-export let tasks = [
-    {
-        text: 'Купить чай',
-    },
-    {
-        text: 'Заварить чай',
-    },
-    {
-        text: 'Выпить чай',
-    },
-]
+export let tasks = [ // данные о задачах получим от сервера
+  // { name: "Купить чай" },
+  // { name: "Заварить чай" },
+  // { name: "Выпить чай" },
+];
+
+export const updateTasks = (newTasks) => {
+  // console.log(newTasks);
+  tasks = newTasks;
+}
